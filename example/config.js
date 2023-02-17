837f2fbd16b4085525d3540bd1ab97dd7c244449
mengyuan <hongwen.chw@antgroup.com>
2023-02-17 16:14:32 +0800

    example: example for naming

--- example/config.js
+++ example/config.js
@@ -2,16 +2,22 @@
 
 const { NacosConfigClient, NacosConfigResponse } = require('../index')
 
+// If it fails, pay attention to err
 const nacos_config_client = new NacosConfigClient({
     serverAddr: '0.0.0.0:8848',
     namespace: "hongwen",
     appName: "binding-node-example-app"
 });
 
-var conf_content = nacos_config_client.getConfig('hongwen.properties', 'LOVE');
-console.log(conf_content);
+try {
+    // If it fails, pay attention to err
+    var conf_content = nacos_config_client.getConfig('hongwen.properties', 'LOVE');
+    console.log(conf_content);
 
-var config_resp = nacos_config_client.getConfigResp('hongwen.properties', 'LOVE');
-console.log(config_resp.content);
+    var config_resp = nacos_config_client.getConfigResp('hongwen.properties', 'LOVE');
+    console.log(config_resp.content);
+} catch(e) {
+    console.log(e);
+}
 
 nacos_config_client.addListener('hongwen.properties', 'LOVE', (err, config_resp) => { console.log(config_resp) });
