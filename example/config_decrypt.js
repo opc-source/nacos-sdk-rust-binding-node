804b91aca405b559ed6a05445daf9679559dde4c
mengyuan <hongwen.chw@antgroup.com>
2023-04-10 14:05:52 +0800

    chore: tip of config_decrypt.js

--- example/config_decrypt.js
+++ example/config_decrypt.js
@@ -12,10 +12,13 @@ const nacos_config_client = new NacosConfigClient(
 (err, config_req, config_resp) => {
     // config_req or config_resp only one not null. e.g. you can do encrypt for config_req, decrypt for config_resp.
     if (config_resp != null) {
-        config_resp.content = "func config_decrypt change it." // TODO by customize and please care about encryptedDataKey whether not null
+        // TODO by customize and please care about encryptedDataKey whether not null
+        //  config_resp.content = aes decrypt config_resp.content with config_resp.encryptedDataKey ??
+        config_resp.content = "func config_decrypt change it."
     }
     if (config_req != null) {
-        // config_req.content = "encrypt content." // TODO by customize and please care about encryptedDataKey whether not null
+        // TODO by customize and please care about encryptedDataKey whether not null
+        //  config_req.content = aes encrypt config_req.content with config_req.encryptedDataKey ??
     }
     // !!! must return them !!!
     return [config_req, config_resp];
