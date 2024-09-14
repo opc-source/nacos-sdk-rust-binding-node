21e820c8fdfecaaea8b844762fd48d707c600da4
CherishCai <785427346@qq.com>
2024-09-14 21:16:12 +0800

    bump: nacos-sdk=0.4.2

--- src/lib.rs
+++ src/lib.rs
@@ -50,10 +50,16 @@ pub struct ClientOptions {
   pub namespace: String,
   /// AppName
   pub app_name: Option<String>,
-  /// Username for Auth
+  /// Username for Auth, Login by Http with Token
   pub username: Option<String>,
-  /// Password for Auth
+  /// Password for Auth, Login by Http with Token
   pub password: Option<String>,
+  /// Access_Key for Auth, Login by Aliyun Ram
+  pub access_key: Option<String>,
+  /// Access_Secret for Auth, Login by Aliyun Ram
+  pub access_secret: Option<String>,
+  /// Signature_Region_Id for Auth, Login by Aliyun Ram
+  pub signature_region_id: Option<String>,
   /// naming push_empty_protection, default true
   pub naming_push_empty_protection: Option<bool>,
   /// naming load_cache_at_start, default false
